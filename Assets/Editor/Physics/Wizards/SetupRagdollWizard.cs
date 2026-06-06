e501d83a8bbf62ebff318636df3a3c8e67eab1bc
anchurcn <809595156@qq.com>
2026-06-06 21:33:01 +0800

    refactor phase 5

--- Assets/Editor/Physics/Wizards/SetupRagdollWizard.cs
+++ Assets/Editor/Physics/Wizards/SetupRagdollWizard.cs
@@ -10,30 +10,6 @@ using GanyuEditor.Physics;
 
 namespace GanyuEditor.Editor.Physics.Wizards
 {
-    internal static class Helper
-    {
-        public static Vector3 Pos(this StudioBone self) => self.transform.position;
-        public static Matrix4x4 Trans(this StudioBone self) => self.transform.localToWorldMatrix;
-
-        public static GameObject RootObject(this GameObject self)
-        {
-            while (self.transform.parent)
-            {
-                self = self.transform.parent.gameObject;
-            }
-            return self;
-        }
-
-        public static void AddShape(this StudioBone self, Matrix4x4 shapeTrans, float height, float radius)
-        {
-            var shape = self.gameObject.AddComponent<CapsuleCollisionShape>();
-            self.gameObject.AddComponent<PhysicsBody>();
-            shape.WorldTransform = shapeTrans;
-            shape.Height = height;
-            shape.Radius = radius;
-        }
-    }
-
     // Bipped ragdoll
     public class SetupRagdollWizard : ScriptableWizard
     {
---
+++ Assets/Scripts/Extensions/StudioBoneExtensions.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using GanyuEditor.Physics;
+
+namespace GanyuEditor.Extensions
+{
+    public static class StudioBoneExtensions
+    {
+        public static Vector3 Pos(this StudioBone self) => self.transform.position;
+
+        public static Matrix4x4 Trans(this StudioBone self) => self.transform.localToWorldMatrix;
+
+        public static GameObject RootObject(this GameObject self)
+        {
+            while (self.transform.parent)
+            {
+                self = self.transform.parent.gameObject;
+            }
+            return self;
+        }
+
+        public static void AddShape(this StudioBone self, Matrix4x4 shapeTrans, float height, float radius)
+        {
+            var shape = self.gameObject.AddComponent<CapsuleCollisionShape>();
+            self.gameObject.AddComponent<PhysicsBody>();
+            shape.WorldTransform = shapeTrans;
+            shape.Height = height;
+            shape.Radius = radius;
+        }
+    }
+}
